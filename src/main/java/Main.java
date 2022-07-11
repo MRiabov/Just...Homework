a47513b155a24649ad465d197cbc491c2454955d
MRiabov <maksymriabov2004@gmail.com>
2022-07-11 09:26:02 +0100

    I honestly don't know how to solve current thread not owner, and I don't have time nor will to do it. The other code works perfectly fine though

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import papers.PaperToPrint;
+import papers.PaperToScan;
+import printNscanner.Device;
+
 public class Main {
+    public static void main(String[] args) {
+        Device device = new Device();
+            device.printQueueAdd(new PaperToPrint("BrandInfo 8800555535",4));
+            device.scanQueueAdd(new PaperToScan("horny stuff",8));
+    }
 }
--- src/main/java/printNscanner/Device.java
+++ src/main/java/printNscanner/Device.java
@@ -7,46 +7,71 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Device{
+    private final Object SCAN_MONITOR=new Object();
+    private final Object PRINT_MONITOR=new Object();
     private final BlockingQueue<PaperToPrint> devicePrintQueue = new LinkedBlockingQueue<>();
     private final BlockingQueue<PaperToScan> deviceScanQueue = new LinkedBlockingQueue<>();
 
     public Device() {
-        print();
+        printInit();
+        scanInit();
     }
 
-    public void printQueueAdd(PaperToPrint paperToPrint, int amount){
-        devicePrintQueue.add(paperToPrint);
+    public void printQueueAdd(PaperToPrint paperToPrint){
+        synchronized (PRINT_MONITOR){
+            devicePrintQueue.add(paperToPrint);
+        }
     }
 
-    public void scanQueueAdd(PaperToScan paperToScan, int amount){
-        deviceScanQueue.add(paperToScan);
+    public void scanQueueAdd(PaperToScan paperToScan){
+        synchronized (SCAN_MONITOR){
+            deviceScanQueue.add(paperToScan);
+        }
     }
 
-    private void print(){
+    private void printInit(){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true) {
-                    PaperToPrint paperToPrint = null;
-                    try {
-                        paperToPrint = devicePrintQueue.take();
-                    } catch (InterruptedException e) { e.printStackTrace();}
-
-                    for (int i = 0; i < paperToPrint.getAmount(); i++) {
-                        System.out.println("Printed N" + i + ". There is " + paperToPrint.getData() + " scribbled on the paper");
+                    while (true) {
+                        PaperToPrint paperToPrint = null;
                         try {
-                            wait(1000);
+                            paperToPrint = devicePrintQueue.take();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        for (int i = 0; i < paperToPrint.getAmount(); i++) {
+                            System.out.println("Printed N" + i + ". There is " + paperToPrint.getData() + " scribbled on the paper");
+                            try {
+                                PRINT_MONITOR.wait(1000);
+                            } catch (InterruptedException ignored) {
+                            }
+                        }
+                    }
+                }
+        }).start();
+    }
+
+    private void scanInit(){
+        Thread scanThread= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    PaperToScan paperToScan = null;
+                    try {
+                        paperToScan = deviceScanQueue.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();}
+                    for (int i = 0; i < paperToScan.getAmount(); i++) {
+                        System.out.println("Scanned N" + i + ". " + paperToScan.getData() + " shows up on the screen");
+                        try {
+                            SCAN_MONITOR.wait(1000);
                         } catch (InterruptedException ignored) {
                         }
                     }
                 }
             }
-        }).start();
-    }
-
-    private void scan(String data, int number){
-        for (int i = 0; i < number; i++) {
-            System.out.println("Scanned N"+i+". "+data+" shows up on the screen");
-        }
+        });
+             scanThread.start();
     }
 }
